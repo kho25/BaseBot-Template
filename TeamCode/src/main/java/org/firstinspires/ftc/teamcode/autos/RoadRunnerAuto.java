56fedb827dcc138de72379a3202ad06e50f72d32
Ayaan Govil <ayaangovil@gmail.com>
2021-11-02 11:00:20 -0700

    roadrunner auto fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RoadRunnerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RoadRunnerAuto.java
@@ -36,7 +36,9 @@ public class RoadRunnerAuto extends LinearOpMode {
     public void runOpMode() {
         Devices.initDevices(hardwareMap);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        drive.setPoseEstimate(new Pose2d(10, 10, Math.toRadians(90)));
+
+        Pose2d startPos = new Pose2d(10, 10, Math.toRadians(90));
+        drive.setPoseEstimate(startPos);
 
         telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
@@ -45,21 +47,21 @@ public class RoadRunnerAuto extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
+        Trajectory traj1 = drive.trajectoryBuilder(startPos)
                 .splineTo(new Vector2d(20, 20), Math.toRadians(90))
                 .build();
         drive.followTrajectory(traj1);
 
         // do something
 
-        Trajectory traj2 = drive.trajectoryBuilder(new Pose2d())
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
                 .strafeLeft(10)
                 .build();
         drive.followTrajectory(traj2);
 
         // do something else
 
-        Trajectory traj3 = drive.trajectoryBuilder(new Pose2d())
+        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
                 .back(10)
                 .build();
         drive.followTrajectory(traj3);
